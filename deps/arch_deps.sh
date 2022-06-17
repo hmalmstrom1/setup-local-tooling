e23f3484719bd877d8cb23609fa4bdbec039d50e
Heath Malmstrom <heath.malmstrom@gmail.com>
2022-06-17 11:15:26 -0500

    fixed arch_deps

--- deps/arch_deps.sh
+++ deps/arch_deps.sh
@@ -1,2 +1,2 @@
 
-sudo pacman -S pass texlive-core texlive-bin texlive-latexextra texlive-lang texlive-fontsextra ghostcript pandoc nodejs npm jdk-openjdk cmake
+sudo pacman -S pass texlive-core texlive-bin texlive-latexextra texlive-fontsextra ghostscript graphviz nodejs npm jre17-openjdk cmake
